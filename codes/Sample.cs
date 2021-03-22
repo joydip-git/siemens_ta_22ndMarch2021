c07d68cf507e4003c7c67001980f6127c95b6325
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-22 11:09:02 +0530

    Notes uploaded

---
+++ codes/Sample.cs
@@ -0,0 +1,19 @@
+using System; //<--namespace, NOT a library directly
+
+class Sample
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello C#");
+		Console.WriteLine("press any key to terminate...");
+		Console.ReadKey();
+	}
+}
+
+/*
+import System;
+
+class Sample
+	static void Main()
+*/
+		
