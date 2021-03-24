f6e1d944681570937b87ccae0c18c3d485cba42d
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-24 12:03:08 +0530

    class object upload

---
+++ codes/day-3/OOPDemos/Class_Object/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Class_Object
+{
+    
+    class Program
+    {
+        static Trainee GetInput()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Marks: ");
+            double marks = double.Parse(Console.ReadLine());
+
+            Console.Write("Department: ");
+            string departmentName = Console.ReadLine();
+
+            Trainee traineeObject = new Trainee(id, name, departmentName, marks);
+            return traineeObject;
+        }
+        static void Main()
+        {
+            //for (int i = 0; i < 2; i++)
+            //{
+            Trainee traineeeReference = GetInput();
+            //traineeeReference.Trainee();
+            string information = traineeeReference.GetInfo();
+            Console.WriteLine(information);
+            //traineeeReference = null;
+            //WeakReference<Trainee> weakReference = new WeakReference<Trainee>(traineeeReference);
+            //SqlConnection
+            //}
+            //code
+            //traineeeReference.GetInfo();
+
+            GC.Collect();
+            GC.Collect();
+        }
+    }
+}
---
+++ codes/day-3/OOPDemos/Class_Object/Trainee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Object
+{
+    class Trainee
+    {
+        private int traineeId;
+        private double traineeMarks;
+        private string traineeName;
+        private string traineeDepartmentName;
+
+        //default parameterless constructor
+        public Trainee()
+        {
+            //int x = 19;
+            //traineeId = 0;
+            //traineeMarks = 0;
+            //traineeName = null;
+            //traineeDepartmentName = null;
+        }
+        //number of args
+        //data type of args
+        //position of args
+        //parajeterized constructor
+        public Trainee(int id, string name, string departmentName, double marks) : this()
+        {
+            //this.traineeId;
+            traineeId = id;
+            traineeName = name;
+            traineeDepartmentName = departmentName;
+            traineeMarks = marks;
+        }
+        public string GetInfo()
+        {
+            return "Name: " + traineeName + ", Id: " + traineeId + ", Department: " + traineeDepartmentName + ", Marks: " + traineeMarks;
+        }
+    }
+}
