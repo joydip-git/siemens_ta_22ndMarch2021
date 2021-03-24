59ce669c0814e4c7a98eaaa770ddf94ac2458a29
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-24 12:51:09 +0530

    Employee class upload

---
+++ codes/day-3/OOPDemos/HRMSApp/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMSApp
+{
+    class Employee
+    {
+        int employeeId;
+        string employeeName;
+        string employeeProjectName;
+        decimal employeeBasicPayment;
+        decimal employeeDaPayemnt;
+        decimal employeeHraPayment;
+        decimal employeeTotalSalary;
+
+        public Employee()
+        {
+
+        }
+
+        public Employee(int employeeId, string employeeName, string employeeProjectName, decimal employeeBasicPayment, decimal employeeDaPayemnt, decimal employeeHraPayment)
+        {
+            this.employeeId = employeeId;
+            this.employeeName = employeeName;
+            this.employeeProjectName = employeeProjectName;
+            this.employeeBasicPayment = employeeBasicPayment;
+            this.employeeDaPayemnt = employeeDaPayemnt;
+            this.employeeHraPayment = employeeHraPayment;
+        }
+
+        public decimal CalculateSalary()
+        {
+            employeeTotalSalary = employeeBasicPayment + employeeDaPayemnt + employeeHraPayment;
+            return employeeTotalSalary;
+        }
+        public string GetInformation()
+        {
+            //string template/ templated string/ string interpolation
+            //C# 6.0 - .Net Frameowrk 4.6
+            return $"Name:{this.employeeName}, Id: {employeeId}, Project: {employeeProjectName}, Salary:{employeeTotalSalary}";
+        }
+    }
+}
---
+++ codes/day-3/OOPDemos/HRMSApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HRMSApp
+{
+    class Program
+    {
+        static Employee CreateEmployee()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Project Name: ");
+            string projectName = Console.ReadLine();
+
+            Console.Write("Basic Pay: ");
+            decimal basicPayment = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Da Pay: ");
+            decimal daPayment = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Hra Pay: ");
+            decimal hraPayment = decimal.Parse(Console.ReadLine());
+
+            //C#-4.0: named arguments (.Net Framework 4.0 - 2010)
+            Employee employeeReference = new Employee(employeeName: name, employeeId: id, employeeBasicPayment: basicPayment, employeeDaPayemnt: daPayment, employeeProjectName: projectName, employeeHraPayment: hraPayment);
+
+            return employeeReference;
+        }
+        static void PrintEmployeeInformation(Employee employee)
+        {
+            employee.CalculateSalary();
+            string information = employee.GetInformation();
+            Console.WriteLine(information);
+        }
+        static void Main()
+        {
+            Employee employeeObjRef = CreateEmployee();
+            PrintEmployeeInformation(employeeObjRef);
+        }
+    }
+}
