1e72830776d05c4c7328b50e8a140a8cb0b4e1fd
siemens-c2c <joy_mon78@yahoo.co.in>
2021-04-01 10:21:29 +0530

    new features update

---
+++ codes/day-9/PMSAPP/PMSAPP.Entities/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace PMSAPP.Entities
+{
+    public class Product
+    {
+        //Backing field
+        // private readonly int productId;
+        //public int ProductId { get => productId; }
+        public int ProductId { set; get; }
+        public string ProductName { set; get; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+
+        public Product()
+        {
+
+        }
+        //public Product(int productId)
+        //{
+        //    this.productId = productId;
+        //}
+        public Product(int id, string name, decimal price, string description) //: this(id)
+        {
+            ProductName = name;
+            Price = price;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return $"Id:{ProductId}, Name:{ProductName}, Price:{Price}, Description:{Description}";
+        }
+    }
+}
---
+++ codes/day-9/PMSAPP/PMSAPP.UserInterface/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PMSAPP.Entities;
+
+namespace PMSAPP.UserInterface
+{
+    class Program
+    {
+        static void Main()
+        {           
+            var products = new List<Product>
+            {
+                new Product { ProductId = 2, ProductName = "Dell XPS", Price = 167000, Description = "new version of laptop from dell" },
+                new Product { ProductId = 1, ProductName = "HP Probook", Price = 145000, Description = "new version of laptop from HP" },
+                new Product { ProductId = 3, ProductName = "Lenovo Beast", Price = 135000, Description = "new version of laptop from Lenovo" }
+            };
+        }
+    }
+}
